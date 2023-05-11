be30721ba008b834635a45bafb72424e33911247
turku <turkucakmak@hotmail.com>
2023-05-11 17:28:34 +0300

    One failing test

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
 // Your code here
+class Cat{
+    constructor(name,sex){
+        this.name = name;
+        this.sex = sex;
+    }
+    speak(){
+        return `${this.name} says meow!`;
+    }
+}
+
+class Dog{
+    constructor(name,sex){
+        this.name = name;
+        this.sex = sex;
+    }
+    speak(){
+        return `${this.name} says woof!`;
+    }
+}
+
+class Bird{
+    
+    constructor(name,sex){
+        this.name = name;
+        this.sex = sex;
+    }
+    speak(sex){
+
+        if(sex=="male"){
+        return `It's me! ${this.name}, the parrot!`;
+        }
+        else{
+            return `Mable says squawk!`;
+        }
+    }
+}
+
+Bird.speak("male")
